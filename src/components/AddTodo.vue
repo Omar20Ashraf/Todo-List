b6d9c60ec800430c8298f1cb4c2347a68a7d549a
Omar Ashraf <omaarashraf2020@gmail.com>
2019-08-06 18:39:47 +0200

    Deploy the project

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from "uuid";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newAdd = {
+        //i no longer need this because json will create id.
+        // id: uuid.v4(),
+        title: this.title,
+        completed: false
+      };
+      //send to the parent
+      this.$emit("add-todo", newAdd);
+
+      //empty the field after submiting the form;
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
+
